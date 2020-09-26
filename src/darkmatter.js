dd25980e77ac2e3df5c9423623d4388579d5d5ce
Parker <23155686+sparkcity@users.noreply.github.com>
2020-09-26 18:58:58 -0400

    v1.1 - stable again, no automation for custom skills

--- src/darkmatter.js
+++ src/darkmatter.js
@@ -1,9 +1,6 @@
 import { DND5E } from '/systems/dnd5e/module/config.js';
 import Actor5e from "/systems/dnd5e/module/actor/entity.js";
 
-DND5E.skills["dat"] = "Data";
-DND5E.skills["pil"] = "Piloting";
-DND5E.skills["tec"] = "Technology";
 DND5E.weaponProficiencies["simBl"] = "Simple Blasters";
 DND5E.weaponProficiencies["marBl"] = "Martial Blasters";
 DND5E.weaponTypes["simpleB"] = "Simple Blaster";
@@ -13,12 +10,76 @@ DND5E.weaponTypes["martialB"] = "Martial Blaster";
 //If the type is vehicle, it makes no changes.
 const prep = Actor5e.prototype.prepareBaseData;	
 function extendActorData() {
-	if(this.data.type === "npc" || this.data.type === "character") {
-		const skl = this.data.data.skills;
-        skl["dat"] = skl["dat"] || {value: 0, ability: "int"};
-        skl["pil"] = skl["pil"] || {value: 0, ability: "dex"};
-		skl["tec"] = skl["tec"] || {value: 0, ability: "int"};
+  console.log(this);
+  const dat = this.data.data;
+	dat["newskills"] = dat["newskills"] || 
+	{ "dat": 
+		{
+			total: ''
+    },
+    "pil": 
+		{
+			total: ''
+    },
+    "tec": 
+		{
+			total: ''
     }
+	};
+
+	if(this.data.type === "npc" || this.data.type === "character") {
+	}
+	else {
+		const atr = this.data.data.attributes;
+		atr["maneuver"] = atr["maneuver"] || 0;
+		atr["engineclass"] = atr["engineclass"] || 0;
+	}
 	return prep.call(this);
 }
 Actor5e.prototype.prepareBaseData = extendActorData;
+
+//Adding elements to CarbonCompact sheet
+Hooks.on("renderActorSheet", (app, html, data) => {
+    
+  if(data.isCharacter || data.isNPC){
+    const skillslist = html.find("section.sheet-body").find("ul.skills-list");
+    skillslist.append(`
+      <li class="skill flexrow dat" data-skill="int">
+        <input type="hidden" name="data.newskills.dat.value" data-dtype="Number">
+        <h4 class="skill-name">Data</h4>
+        <span class="skill-ability custom">Int</span>
+        <span class="skill-mod custom"><input name="data.newskills.dat.total" type="text" value="${data.data.newskills.dat.total}" data-dtype="Text" placeholder="+0"/></span>
+      </li>
+
+      <li class="skill flexrow pil" data-skill="dex">
+      <input type="hidden" name="data.newskills.pil.value" data-dtype="Number">
+      <h4 class="skill-name">Piloting</h4>
+      <span class="skill-ability custom">Dex</span>
+      <span class="skill-mod custom"><input name="data.newskills.pil.total" type="text" value="${data.data.newskills.pil.total}" data-dtype="Text" placeholder="+0"/></span>
+      </li>
+
+      <li class="skill flexrow tec" data-skill="int">
+      <input type="hidden" name="data.newskills.tec.value" data-dtype="Number">
+      <h4 class="skill-name">Technology</h4>
+      <span class="skill-ability custom">Int</span>
+      <span class="skill-mod custom"><input name="data.newskills.tec.total" type="text" value="${data.data.newskills.tec.total}" data-dtype="Text" placeholder="+0"/></span>
+      </li>
+    `); 
+  }
+
+else{
+    const traits = html.find("div.traits");
+    traits.prepend(`
+    <div class="form-group {{#unless data.attributes.maneuver}}inactive{{/unless}}">
+      <label>Maneuverability</label>
+      <input type="text" name="data.attributes.maneuver" data-dtype="Number" placeholder="0"
+        value="${data.data.attributes.maneuver}" />
+    </div>
+    <div class="form-group {{#unless data.attributes.engineclass}}inactive{{/unless}}">
+      <label>Engine Class</label>
+      <input type="text" name="data.attributes.engineclass" data-dtype="Number" placeholder="0"
+        value="${data.data.attributes.engineclass}" />
+    </div>
+    `);
+  }
+});
